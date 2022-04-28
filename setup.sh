fd2a592a3d487108d88b9b6203b1c15980bdca3e
kai-franz <kfranz@andrew.cmu.edu>
2022-04-28 11:16:51 -0400

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 sudo apt update
 sudo apt install make
 sudo apt install llvm
+sudo apt install clang
+sudo apt install lib32readline-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install flex bison
+./configure --with-llvm --prefix=$HOME/my_postgres --without-readline
