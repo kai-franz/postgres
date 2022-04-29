db2b3b8f7f7e18766d5d466e3907604f6c79bed0
kai-franz <kfranz@andrew.cmu.edu>
2022-04-29 15:29:05 -0400

    Added packages to setup scripts

--- setup.sh
+++ setup.sh
@@ -1,8 +1,3 @@
 sudo apt update
-sudo apt install make
-sudo apt install llvm
-sudo apt install clang
-sudo apt install lib32readline-dev
-sudo apt-get install zlib1g-dev
-sudo apt-get install flex bison
-./configure --with-llvm --prefix=$HOME/my_postgres --without-readline
+sudo apt install make llvm clang lib32readline-dev zlib1g-dev flex bison
+./configure --with-llvm --prefix=$HOME/my_postgres2 --without-readline
